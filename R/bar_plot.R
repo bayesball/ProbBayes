7dec4ca67646ea4aa1a6b7962951a28fd210e79d
Jim Albert <albertcb1@gmail.com>
2016-12-06 21:16:52 -0500

    added bar plot function

---
+++ R/bar_plot.R
@@ -0,0 +1,17 @@
+bar_plot <- function(y, ...){
+  TH <- theme(
+    plot.title = element_text(
+      colour = "blue",
+      size = 18,
+      hjust = 0.5,
+      vjust = 0.8,
+      angle = 0
+    )
+  )
+  p <- ggplot(data.frame(Y=y), aes(Y)) +
+  geom_bar(width=0.5, fill="red") +
+  ylab("Frequency")
+  if (nargs() == 2)
+    p <- p + ggtitle(...) + TH
+  p
+}
