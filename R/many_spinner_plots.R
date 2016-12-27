786931cca9e79f874b86e29aded32d3dc554cc37
Jim Albert <albertcb1@gmail.com>
2016-12-27 14:24:01 -0500

    Added function

---
+++ R/many_spinner_plots.R
@@ -0,0 +1,16 @@
+many_spinner_plots <- function(list_regions){
+  n <- length(list_regions)
+  if(n > 6) stop("Too many spinners to plot")
+  Letters <- c("A", "B", "C", "D", "E", "F", "G", "H", "I")
+  plots <- vector(mode = "list", length = n)
+  for(j in 1:n){
+    plots[[j]] <- spinner_plot2(list_regions[[j]],
+                       title=paste("Spinner", Letters[j]))
+  }
+  if (n == 2) rows.cols <- c(1, 2) else
+    if (n %in% c(3, 4)) rows.cols <- c(2, 2) else
+      if (n %in% c(5, 6)) rows.cols <- c(3, 2)
+
+  grid.arrange(grobs=plots,
+        nrow=rows.cols[1], ncol=rows.cols[2])
+}
