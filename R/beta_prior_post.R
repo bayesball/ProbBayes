e7a415a049b6cb0ee7d2f4f8d8194b942c734e28
Jim Albert <albertcb1@gmail.com>
2016-12-29 21:33:23 -0500

    editing function

--- R/beta_prior_post.R
+++ R/beta_prior_post.R
@@ -1,4 +1,6 @@
-beta_prior_post <- function(prior_shapes, post_shapes){
+beta_prior_post <- function(prior_shapes, post_shapes,
+                            label_1="Prior",
+                            label_2="Posterior"){
   TH <- theme(
     plot.title = element_text(
       colour = "blue",
@@ -10,15 +12,14 @@ beta_prior_post <- function(prior_shapes, post_shapes){
   )
 ggplot(data.frame(x=c(0, 1)), aes(x)) +
   stat_function(fun=dbeta, geom="line",
-                aes(color="Prior"), size=1.5,
+                aes(color=label_1), size=1.5,
                 args=list(shape1=prior_shapes[1],
                           shape2=prior_shapes[2])) +
   stat_function(fun=dbeta, geom="line",
-                aes(color="Posterior"), size=1.5,
+                aes(color=label_2), size=1.5,
                 args=list(shape1=post_shapes[1],
                           shape2=post_shapes[2])) +
   scale_colour_manual(values=c("red", "blue")) +
   labs(colour = "Type") +
-  ggtitle("Prior-Posterior Plot") + TH +
   xlab("P") + ylab("Density")
 }
