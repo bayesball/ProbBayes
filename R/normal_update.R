63d9819ab8e1c17a0012b2cecacf9a31302de3a3
Jim Albert <albertcb1@gmail.com>
2017-01-03 08:31:02 -0500

    Edited function

--- R/normal_update.R
+++ R/normal_update.R
@@ -1,4 +1,4 @@
-normal_update <- function(prior, data){
+normal_update <- function(prior, data, teach=FALSE){
   prior_mean <- prior[1]
   prior_sd <- prior[2]
   data_mean <- data[1]
@@ -9,5 +9,11 @@ normal_update <- function(prior, data){
   post_sd <- sqrt(1 / post_precision)
   post_mean <- weighted.mean(c(prior_mean, data_mean),
               c(prior_precision, data_precision))
+  if(teach==TRUE)
+    data.frame(Type=c("Prior", "Data", "Posterior"),
+             Mean=c(prior_mean, data_mean, post_mean),
+             Precision=c(prior_precision, data_precision,
+                         post_precision),
+             Stand_Dev=c(prior_sd, data_se, post_sd)) else
   c(post_mean, post_sd)
 }
