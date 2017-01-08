be9ceffe5a5bb36c5d6103951fd7a57153fcfc64
Jim Albert <albertcb1@gmail.com>
2017-01-08 17:16:24 -0500

    edited prob_plot

--- R/prob_plot.R
+++ R/prob_plot.R
@@ -2,7 +2,7 @@ prob_plot <- function(d){
   p <- ggplot(d,
       aes_string(colnames(d)[1], colnames(d)[2])) +
     geom_segment(aes_string(xend = colnames(d)[1], yend = 0),
-                 size = 10,
+                 size = 15,
                  lineend = "butt",
                  color="red")
   p
