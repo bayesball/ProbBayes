881c707ddd5ddb3ca346506579b4249e91345b67
Jim Albert <albertcb1@gmail.com>
2017-02-17 18:23:20 -0500

    A few changes

--- R/testing_prior.R
+++ R/testing_prior.R
@@ -1,13 +1,14 @@
-testing_prior <- function(lo=.1, hi=.9, np=9,
+testing_prior <- function(lo=.1, hi=.9, n_values=9,
                           pequal=0.5, uniform=FALSE){
-  p1 <- seq(lo, hi, length = np)
+  p1 <- seq(lo, hi, length = n_values)
   p2 <- p1
-  n_diagonal <- np
-  n_off_diag <- np ^ 2 - np
-  prior <- matrix(0, np, np) + (1 - pequal) / n_off_diag
-  diag(prior) <-  pequal / np
+  n_diagonal <- n_values
+  n_off_diag <- n_values ^ 2 - n_values
+  prior <- matrix(0, n_values, n_values) +
+    (1 - pequal) / n_off_diag
+  diag(prior) <-  pequal / n_values
   if(uniform==TRUE)
-    prior <- 0 * prior + 1 / np ^ 2
+    prior <- 0 * prior + 1 / n_values ^ 2
   dimnames(prior)[[1]] <- p1
   dimnames(prior)[[2]] <- p2
   prior
