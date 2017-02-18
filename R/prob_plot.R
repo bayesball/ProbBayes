31ed0300a1db5bcff2c65d7c4645ba5104d76cc1
Jim Albert <albertcb1@gmail.com>
2017-02-17 21:02:11 -0500

    edit prob_plot function

--- R/prob_plot.R
+++ R/prob_plot.R
@@ -1,8 +1,12 @@
 prob_plot <- function(d){
+  N <- dim(d)[1]
+  Size <- 100 / N
+  Size <- ifelse(Size > 15, 15, Size)
+  Size <- ifelse(Size < 2, 2, Size)
   p <- ggplot(d,
       aes_string(colnames(d)[1], colnames(d)[2])) +
     geom_segment(aes_string(xend = colnames(d)[1], yend = 0),
-                 size = 15,
+                 size = Size,
                  lineend = "butt",
                  color="red")
   p
