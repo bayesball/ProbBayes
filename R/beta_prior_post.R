438122762a2fb39e913c7e4c7dd7025e1789373e
Jim Albert <albertcb1@gmail.com>
2020-01-29 16:59:14 -0500

    revise one function

--- R/beta_prior_post.R
+++ R/beta_prior_post.R
@@ -1,6 +1,4 @@
-beta_prior_post <- function(prior_shapes, post_shapes,
-                            label_1="Prior",
-                            label_2="Posterior"){
+beta_prior_post <- function(prior_shapes, post_shapes){
   TH <- theme(
     plot.title = element_text(
       colour = "blue",
@@ -10,6 +8,8 @@ beta_prior_post <- function(prior_shapes, post_shapes,
       angle = 0
     )
   )
+label_1 <- "1 Prior"
+label_2 <- "2 Posterior"
 x <- NULL
 ggplot(data.frame(x=c(0, 1)), aes(x)) +
   stat_function(fun=dbeta, geom="line",
@@ -20,8 +20,8 @@ ggplot(data.frame(x=c(0, 1)), aes(x)) +
                 aes(color=label_2), size=1.5,
                 args=list(shape1=post_shapes[1],
                           shape2=post_shapes[2])) +
-  scale_colour_manual(values=c("blue", "red"),
-                      labels = c(label_1, label_2)) +
+  scale_colour_manual(values = c("blue", "red"),
+                      labels = c("Prior", "Posterior")) +
   labs(colour = "Type") +
   xlab("P") + ylab("Density")
 }
